71fcc0349528c1d7c3448d0c13a1e6f6c47196a9
kemp sayson <kemp.taisondigital@gmail.com>
2019-07-28 15:06:49 +0800

    added dashboard

---
+++ src/components/dashboard/DashboardCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="dashboard-card">
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-card {
+    width: 400px;
+    border: 1px solid #fff;
+    padding: 25px;
+}
+</style>
--- src/views/home/dashboard/Dashboard.vue
+++ src/views/home/dashboard/Dashboard.vue
@@ -1,14 +1,35 @@
 <template>
-	<div class="home">
-        DASHBOARD HEHE
+	<div class="dashboard">
+		<div class="dashboard-container">
+        	<DashboardCard />
+
+			<DashboardCard />
+
+			<DashboardCard />
+		</div>
 	</div>
 </template>
 
 <script>
-
 export default {
 	name: 'home',
+
 	components: {
+		DashboardCard: () => import('@/components/dashboard/DashboardCard.vue')
 	}
 }
 </script>
+
+<style lang="scss" scoped>
+.dashboard {
+	width: 100%;
+	
+	.dashboard-container {
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+	}
+}
+
+</style>
+
