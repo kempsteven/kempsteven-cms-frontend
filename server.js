bda9522c2d67a5db4c35064d00fd31a022c98962
kemp sayson <kemp.taisondigital@gmail.com>
2019-09-11 21:33:04 +0800

    Fix Heroku Deployment

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const serveStatic = require('serve-static')
-const history = require('connect-history-api-fallback');
+// const serveStatic = require('serve-static')
 
-app = express()
+// app = express()
 
-app.use(serveStatic(__dirname + "/dist"))
-
+// app.use(serveStatic(__dirname + "/dist"))
+const path = require('path')
+const history = require('connect-history-api-fallback')
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+app.use(staticFileMiddleware);
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}));
 
 const port = process.env.PORT || 5000
 app.listen(port)
