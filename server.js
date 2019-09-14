8cb7d40e988dbcf77317880c4a978d96da4260da
kemp sayson <kemp.taisondigital@gmail.com>
2019-09-14 13:09:56 +0800

    Fix heroku deployment

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-const express = require('express')
-// const serveStatic = require('serve-static')
+// const express = require('express')
+// // const serveStatic = require('serve-static')
 
-// app = express()
+// // app = express()
 
-// app.use(serveStatic(__dirname + "/dist"))
-const path = require('path')
-const history = require('connect-history-api-fallback')
-const app = express()
+// // app.use(serveStatic(__dirname + "/dist"))
+// const path = require('path')
+// const history = require('connect-history-api-fallback')
+// const app = express()
+// const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+// app.use(staticFileMiddleware);
+// app.use(history({
+//     disableDotRule: true,
+//     verbose: true
+// }));
+
+// app.get('/', function (req, res) {
+//     res.render(path.join(__dirname + '/dist/index.html'));
+// });
+
+// const port = process.env.PORT || 5000
+// app.listen(port)
+// console.log('server started in port ' + port)
+
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
+
+const app = express();
+
 const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+
 app.use(staticFileMiddleware);
 app.use(history({
     disableDotRule: true,
     verbose: true
 }));
+app.use(staticFileMiddleware);
 
 app.get('/', function (req, res) {
     res.render(path.join(__dirname + '/dist/index.html'));
 });
 
-const port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started in port ' + port)
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
