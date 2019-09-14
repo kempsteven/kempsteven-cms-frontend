3698abf89f12cf10ca205bd2c103f591ecc56f2b
kemp sayson <kemp.taisondigital@gmail.com>
2019-09-14 13:16:34 +0800

    Fix heroku deployment

--- server.js
+++ server.js
@@ -30,11 +30,12 @@ const app = express();
 
 const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-app.use(staticFileMiddleware);
 app.use(history({
     disableDotRule: true,
     verbose: true
 }));
+
+app.use(staticFileMiddleware);
 
 app.get('/', function (req, res) {
     res.render(path.join(__dirname + '/dist/index.html'));
